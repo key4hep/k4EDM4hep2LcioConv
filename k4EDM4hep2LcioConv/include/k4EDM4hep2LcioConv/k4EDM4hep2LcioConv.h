d3ffdf17734d49c434782cd20d20a863be7bec7e
Placido Fernandez Declara <placido.fernandez@cern.ch>
2022-04-11 16:38:34 +0200

    update clang-format and apply

--- k4EDM4hep2LcioConv/include/k4EDM4hep2LcioConv/k4EDM4hep2LcioConv.h
+++ k4EDM4hep2LcioConv/include/k4EDM4hep2LcioConv/k4EDM4hep2LcioConv.h
@@ -67,68 +67,78 @@
 #include <UTIL/CellIDEncoder.h>
 #include <lcio.h>
 
-template <typename T1, typename T2> using vec_pair = std::vector<std::pair<T1, T2>>;
+template<typename T1, typename T2>
+using vec_pair = std::vector<std::pair<T1, T2>>;
 
 struct CollectionsPairVectors {
-  vec_pair<lcio::TrackImpl*, edm4hep::Track>                                 tracks;
-  vec_pair<lcio::TrackerHitImpl*, edm4hep::TrackerHit>                       trackerhits;
-  vec_pair<lcio::SimTrackerHitImpl*, edm4hep::SimTrackerHit>                 simtrackerhits;
-  vec_pair<lcio::CalorimeterHitImpl*, edm4hep::CalorimeterHit>               calohits;
-  vec_pair<lcio::RawCalorimeterHitImpl*, edm4hep::RawCalorimeterHit>         rawcalohits;
-  vec_pair<lcio::SimCalorimeterHitImpl*, edm4hep::SimCalorimeterHit>         simcalohits;
-  vec_pair<lcio::TPCHitImpl*, edm4hep::TPCHit>                               tpchits;
-  vec_pair<lcio::ClusterImpl*, edm4hep::Cluster>                             clusters;
-  vec_pair<lcio::VertexImpl*, edm4hep::Vertex>                               vertices;
+  vec_pair<lcio::TrackImpl*, edm4hep::Track> tracks;
+  vec_pair<lcio::TrackerHitImpl*, edm4hep::TrackerHit> trackerhits;
+  vec_pair<lcio::SimTrackerHitImpl*, edm4hep::SimTrackerHit> simtrackerhits;
+  vec_pair<lcio::CalorimeterHitImpl*, edm4hep::CalorimeterHit> calohits;
+  vec_pair<lcio::RawCalorimeterHitImpl*, edm4hep::RawCalorimeterHit> rawcalohits;
+  vec_pair<lcio::SimCalorimeterHitImpl*, edm4hep::SimCalorimeterHit> simcalohits;
+  vec_pair<lcio::TPCHitImpl*, edm4hep::TPCHit> tpchits;
+  vec_pair<lcio::ClusterImpl*, edm4hep::Cluster> clusters;
+  vec_pair<lcio::VertexImpl*, edm4hep::Vertex> vertices;
   vec_pair<lcio::ReconstructedParticleImpl*, edm4hep::ReconstructedParticle> recoparticles;
-  vec_pair<lcio::MCParticleImpl*, edm4hep::MCParticle>                       mcparticles;
+  vec_pair<lcio::MCParticleImpl*, edm4hep::MCParticle> mcparticles;
 };
 
-lcio::LCCollectionVec* convTracks(const edm4hep::TrackCollection* const                       tracks_coll,
-                                  vec_pair<lcio::TrackImpl*, edm4hep::Track>&                 tracks_vec,
-                                  const vec_pair<lcio::TrackerHitImpl*, edm4hep::TrackerHit>& trackerhits_vec);
+lcio::LCCollectionVec* convTracks(
+  const edm4hep::TrackCollection* const tracks_coll,
+  vec_pair<lcio::TrackImpl*, edm4hep::Track>& tracks_vec,
+  const vec_pair<lcio::TrackerHitImpl*, edm4hep::TrackerHit>& trackerhits_vec);
 
-lcio::LCCollectionVec* convTrackerHits(const edm4hep::TrackerHitCollection* const            trackerhits_coll,
-                                       const std::string                                     cellIDstr,
-                                       vec_pair<lcio::TrackerHitImpl*, edm4hep::TrackerHit>& trackerhits_vec);
+lcio::LCCollectionVec* convTrackerHits(
+  const edm4hep::TrackerHitCollection* const trackerhits_coll,
+  const std::string cellIDstr,
+  vec_pair<lcio::TrackerHitImpl*, edm4hep::TrackerHit>& trackerhits_vec);
 
 lcio::LCCollectionVec* convSimTrackerHits(
-    const edm4hep::SimTrackerHitCollection* const simtrackerhits_coll, const std::string cellIDstr,
-    vec_pair<lcio::SimTrackerHitImpl*, edm4hep::SimTrackerHit>& simtrackerhits_vec,
-    const vec_pair<lcio::MCParticleImpl*, edm4hep::MCParticle>& mcparticles_vec);
+  const edm4hep::SimTrackerHitCollection* const simtrackerhits_coll,
+  const std::string cellIDstr,
+  vec_pair<lcio::SimTrackerHitImpl*, edm4hep::SimTrackerHit>& simtrackerhits_vec,
+  const vec_pair<lcio::MCParticleImpl*, edm4hep::MCParticle>& mcparticles_vec);
 
-lcio::LCCollectionVec* convCalorimeterHits(const edm4hep::CalorimeterHitCollection* const                calohit_coll,
-                                           const std::string                                             cellIDstr,
-                                           vec_pair<lcio::CalorimeterHitImpl*, edm4hep::CalorimeterHit>& calo_hits_vec);
+lcio::LCCollectionVec* convCalorimeterHits(
+  const edm4hep::CalorimeterHitCollection* const calohit_coll,
+  const std::string cellIDstr,
+  vec_pair<lcio::CalorimeterHitImpl*, edm4hep::CalorimeterHit>& calo_hits_vec);
 
 lcio::LCCollectionVec* convRawCalorimeterHits(
-    const edm4hep::RawCalorimeterHitCollection* const                   rawcalohit_coll,
-    vec_pair<lcio::RawCalorimeterHitImpl*, edm4hep::RawCalorimeterHit>& raw_calo_hits_vec);
+  const edm4hep::RawCalorimeterHitCollection* const rawcalohit_coll,
+  vec_pair<lcio::RawCalorimeterHitImpl*, edm4hep::RawCalorimeterHit>& raw_calo_hits_vec);
 
 lcio::LCCollectionVec* convSimCalorimeterHits(
-    const edm4hep::SimCalorimeterHitCollection* const simcalohit_coll, const std::string cellIDstr,
-    vec_pair<lcio::SimCalorimeterHitImpl*, edm4hep::SimCalorimeterHit>& sim_calo_hits_vec,
-    const vec_pair<lcio::MCParticleImpl*, edm4hep::MCParticle>&         mcparticles);
+  const edm4hep::SimCalorimeterHitCollection* const simcalohit_coll,
+  const std::string cellIDstr,
+  vec_pair<lcio::SimCalorimeterHitImpl*, edm4hep::SimCalorimeterHit>& sim_calo_hits_vec,
+  const vec_pair<lcio::MCParticleImpl*, edm4hep::MCParticle>& mcparticles);
 
-lcio::LCCollectionVec* convTPCHits(const edm4hep::TPCHitCollection* const        tpchit_coll,
-                                   vec_pair<lcio::TPCHitImpl*, edm4hep::TPCHit>& tpc_hits_vec);
+lcio::LCCollectionVec* convTPCHits(
+  const edm4hep::TPCHitCollection* const tpchit_coll,
+  vec_pair<lcio::TPCHitImpl*, edm4hep::TPCHit>& tpc_hits_vec);
 
-lcio::LCCollectionVec* convClusters(const edm4hep::ClusterCollection* const                             cluster_coll,
-                                    vec_pair<lcio::ClusterImpl*, edm4hep::Cluster>&                     cluster_vec,
-                                    const vec_pair<lcio::CalorimeterHitImpl*, edm4hep::CalorimeterHit>& calohits_vec);
+lcio::LCCollectionVec* convClusters(
+  const edm4hep::ClusterCollection* const cluster_coll,
+  vec_pair<lcio::ClusterImpl*, edm4hep::Cluster>& cluster_vec,
+  const vec_pair<lcio::CalorimeterHitImpl*, edm4hep::CalorimeterHit>& calohits_vec);
 
 lcio::LCCollectionVec* convVertices(
-    const edm4hep::VertexCollection* const vertex_coll, vec_pair<lcio::VertexImpl*, edm4hep::Vertex>& vertex_vec,
-    const vec_pair<lcio::ReconstructedParticleImpl*, edm4hep::ReconstructedParticle>& recoparticles_vec);
+  const edm4hep::VertexCollection* const vertex_coll,
+  vec_pair<lcio::VertexImpl*, edm4hep::Vertex>& vertex_vec,
+  const vec_pair<lcio::ReconstructedParticleImpl*, edm4hep::ReconstructedParticle>& recoparticles_vec);
 
 lcio::LCCollectionVec* convReconstructedParticles(
-    const edm4hep::ReconstructedParticleCollection* const                       recos_coll,
-    vec_pair<lcio::ReconstructedParticleImpl*, edm4hep::ReconstructedParticle>& recoparticles_vec,
-    const vec_pair<lcio::TrackImpl*, edm4hep::Track>&                           tracks_vec,
-    const vec_pair<lcio::VertexImpl*, edm4hep::Vertex>&                         vertex_vec,
-    const vec_pair<lcio::ClusterImpl*, edm4hep::Cluster>&                       clusters_vec);
+  const edm4hep::ReconstructedParticleCollection* const recos_coll,
+  vec_pair<lcio::ReconstructedParticleImpl*, edm4hep::ReconstructedParticle>& recoparticles_vec,
+  const vec_pair<lcio::TrackImpl*, edm4hep::Track>& tracks_vec,
+  const vec_pair<lcio::VertexImpl*, edm4hep::Vertex>& vertex_vec,
+  const vec_pair<lcio::ClusterImpl*, edm4hep::Cluster>& clusters_vec);
 
-lcio::LCCollectionVec* convMCParticles(const edm4hep::MCParticleCollection* const            mcparticle_coll,
-                                       vec_pair<lcio::MCParticleImpl*, edm4hep::MCParticle>& mc_particles_vec);
+lcio::LCCollectionVec* convMCParticles(
+  const edm4hep::MCParticleCollection* const mcparticle_coll,
+  vec_pair<lcio::MCParticleImpl*, edm4hep::MCParticle>& mc_particles_vec);
 
 void FillMissingCollections(CollectionsPairVectors& collection_pairs);
 
