e823a1b4c64c1533206a14de5c8cd07e8e1d79f3
Finn Johannsen <finn.johannsen@desy.de>
2022-10-06 13:40:14 +0200

    tted the file using clang format

--- k4EDM4hep2LcioConv/src/k4EDM4hep2LcioConv.cpp
+++ k4EDM4hep2LcioConv/src/k4EDM4hep2LcioConv.cpp
@@ -11,12 +11,12 @@ lcio::LCCollectionVec* convTracks(
   auto* tracks = new lcio::LCCollectionVec(lcio::LCIO::TRACK);
 
   // Loop over EDM4hep tracks converting them to lcio tracks.
-  
+
   for (const auto& edm_tr : (*tracks_coll)) {
     if (edm_tr.isAvailable()) {
       auto* lcio_tr = new lcio::TrackImpl();
-      // The Type of the Tracks need to be set bitwise in LCIO since the setType(int) function is private for the LCIO TrackImpl
-      // and only a setTypeBit(bitnumber) function can be used to set the Tzpe bit by bit.
+      // The Type of the Tracks need to be set bitwise in LCIO since the setType(int) function is private for the LCIO
+      // TrackImpl and only a setTypeBit(bitnumber) function can be used to set the Tzpe bit by bit.
       int type = edm_tr.getType();
       for (int i = 0; i < sizeof(int) * 8; i++) {
         if (type & (1 << i)) {
