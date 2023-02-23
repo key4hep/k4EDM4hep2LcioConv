581300872320d4d9cc2d38dfae0d27204a7d28dc
Finn Johannsen <finn.johannsen@desy.de>
2023-02-23 17:29:01 +0100

    Add executable for comparing lcio and edm4hep file contents

---
+++ tests/compare_contents.cpp
@@ -0,0 +1,77 @@
+#include "CompareEDM4hepLCIO.h"
+
+#include "podio/ROOTFrameReader.h"
+#include "podio/Frame.h"
+
+#include <IOIMPL/LCFactory.h>
+
+#include <iostream>
+
+#define ASSERT_COMPARE_OR_EXIT(collType)                   \
+  if (type == #collType) {                                 \
+    auto& edmcoll = edmEvent.get<collType>(name);          \
+    if (!compare(lcioColl, edmcoll)) {                     \
+      std::cerr << "in collection: " << name << std::endl; \
+      return 1;                                            \
+    }                                                      \
+  }
+
+constexpr auto usageMsg = R"(usage: compare-contents lciofile edm4hepfile)";
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3) {
+    std::cerr << usageMsg << std::endl;
+    return 1;
+  }
+
+  auto lcreader = IOIMPL::LCFactory::getInstance()->createLCReader();
+  lcreader->open(argv[1]);
+
+  auto edmreader = podio::ROOTFrameReader();
+  edmreader.openFile(argv[2]);
+
+  // loop going over every name of the lcio collection and checking if a
+  // collection with the same name exists in the edm file.
+  std::cout << "number of Events" << edmreader.getEntries("events") << std::endl;
+  for (size_t n = 0; n < edmreader.getEntries("events"); ++n) {
+    if (n % 1000 == 0) {
+      std::cout << "Event number: " << n << std::endl;
+    }
+
+    auto lcEvent = lcreader->readNextEvent();
+    auto edmEvent = podio::Frame(edmreader.readNextEntry("events"));
+
+    for (const auto& name : *(lcEvent->getCollectionNames())) {
+      const auto lcioColl = lcEvent->getCollection(name);
+      // TODO: The Frame needs to improve here in order to get to the type
+      // without retrieving the collection
+      const auto& type = [&edmEvent, &name]() {
+        const auto coll = edmEvent.get(name);
+        if (coll) {
+          return coll->getTypeName();
+        }
+        static std::string empty = "";
+        return empty;
+      }();
+      if (type.empty()) {
+        std::cerr << "Collection " << name << " not present in edm4hep file" << std::endl;
+        return 1;
+      }
+
+      ASSERT_COMPARE_OR_EXIT(edm4hep::MCParticleCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::ReconstructedParticleCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::TrackCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::TrackerHitCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::SimTrackerHitCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::CalorimeterHitCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::RawCalorimeterHitCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::SimCalorimeterHitCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::TPCHitCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::ClusterCollection)
+      ASSERT_COMPARE_OR_EXIT(edm4hep::VertexCollection)
+    }
+  }
+
+  return 0;
+}
