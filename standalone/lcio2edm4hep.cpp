76216a96f67ba340acb15246981fc2e4000c1359
Finn Johannsen <finn.johannsen@desy.de>
2023-04-03 11:48:28 +0200

    ome debug messages

--- standalone/lcio2edm4hep.cpp
+++ standalone/lcio2edm4hep.cpp
@@ -86,14 +86,17 @@ int main(int argc, char* argv[])
 
   std::cout << "starting Conversion" << std::endl;
   for (auto i = 0u; i < lcreader->getNumberOfEvents(); ++i) {
-    // for (auto i = 0u; i < 10; ++i) {
-    if (i % 100 == 0) {
+  //for (auto i = 0u; i < 4; ++i) {
+    if (i % 10 == 0) {
       std::cout << "processing Event: " << i << std::endl;
     }
     auto evt = lcreader->readNextEvent();
+    std::cout<<"read Event"<<std::endl;
     // Patching the Event to make sure all events contain the same Collections.
     if (patching == true) {
+      std::cout<<"starting to patch"<<std::endl;
       colPatcher.patchCollections(evt);
+      std::cout<<"patched"<<std::endl;
     }
     const auto edmEvent = LCIO2EDM4hepConv::convertEvent(evt);
     writer.writeFrame(edmEvent, "events");
