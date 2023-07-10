24dcc6975b814558605740b30445cacf5ad57e5b
tmadlener <thomas.madlener@desy.de>
2023-07-10 11:52:35 +0200

    Rework argument parsing of lcio2edm4hep

--- standalone/lcio2edm4hep.cpp
+++ standalone/lcio2edm4hep.cpp
@@ -9,10 +9,11 @@
 
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
-#include <set>
 #include <utility>
+#include <cstdlib>
 
 std::vector<std::pair<std::string, std::string>> getNamesAndTypes(const std::string& collTypeFile)
 {
@@ -39,56 +40,98 @@ std::vector<std::pair<std::string, std::string>> getNamesAndTypes(const std::str
   return names_types;
 }
 
-constexpr auto usageMsg = R"(usage: lcio2edm4hep [-h] inputfile outputfile [colltypefile] [-s] [n])";
+constexpr auto usageMsg = R"(usage: lcio2edm4hep [-h] inputfile outputfile [colltypefile] [-n N])";
+
 constexpr auto helpMsg = R"(
 Convert an LCIO file to EDM4hep
 
 positional arguments:
   inputfile         the input LCIO file
   outputfile        the output EDM4hep file that will be created
-  colltypefile      an optional input file that specifies the names and types of collections
-  -s                if -s is appended, only the collections in 'colltypefile' are written
-                    otherwise all the specified collections are written
-  n                 optionally limit the number of events to n (only w/ -s option)
+  colltypefile      An optional input file that specifies the names and types of
+                    collections that should be present in the output.
 
 optional arguments:
   -h, --help        show this help message and exit
+  -n N              Limit the number of events to convert to N (default = -1, all events)
 
 Examples:
 - print this message:
 lcio2edm4hep -h
 - convert complete file (needs all lcio collections to be present in all files):
 lcio2edm4hep infile.slcio outfile_edm4hep.root
-- the same but providing complete set of collections:
+- the same but providing complete set of collections (either to patch collections in,
+  or to only convert a subset):
 lcio2edm4hep infile.slcio outfile_edm4hep.root coltype.txt
-- write only 42 events with the subset of collections specified in coltype.txt:
-lcio2edm4hep infile.slcio outfile_edm4hep.root coltype.txt -s 42
-
 )";
+
+struct ParsedArgs {
+  std::string inputFile {};
+  std::string outputFile {};
+  std::string patchFile {};
+  int nEvents {-1};
+};
+
+void printUsageAndExit()
+{
+  std::cerr << usageMsg << std::endl;
+  std::exit(1);
+}
+
+ParsedArgs parseArgs(std::vector<std::string> argv)
+{
+  // find help
+  if (std::find_if(argv.begin(), argv.end(), [](const auto& elem) {
+        return elem == "-h" || elem == "--help";
+      }) != argv.end()) {
+    std::cerr << usageMsg << '\n' << helpMsg << std::endl;
+    std::exit(0);
+  }
+
+  auto argc = argv.size();
+  if (argc < 3 || argc > 7) {
+    printUsageAndExit();
+  }
+
+  ParsedArgs args;
+  auto nEventIt = std::find(argv.begin(), argv.end(), "-n");
+  if (nEventIt != argv.end()) {
+    const auto index = std::distance(argv.begin(), nEventIt);
+    if (index > argc - 2) {
+      // No argument left to parse
+      printUsageAndExit();
+    }
+    const auto& value = argv[index + 1]; // get the actual value
+    try {
+      args.nEvents = std::stoi(value);
+    } catch (std::invalid_argument& err) {
+      std::cerr << "Cannot parse " << value << " as an integer" << std::endl;
+      printUsageAndExit();
+    }
+    argv.erase(nEventIt, nEventIt + 2);
+  }
+
+  argc = argv.size();
+  if (argc < 3) {
+    printUsageAndExit();
+  }
+  args.inputFile = argv[1];
+  args.outputFile = argv[2];
+  if (argc == 4) {
+    args.patchFile = argv[3];
+  }
+  return args;
+}
 
 int main(int argc, char* argv[])
 {
-  if (argc == 2 && (argv[1] == std::string("-h") || argv[1] == std::string("--help"))) {
-    std::cerr << usageMsg << '\n' << helpMsg << std::endl;
-    return 0;
-  }
+  const auto args = parseArgs({argv, argv + argc});
 
-  bool createSubset = false;
-
-  if (argc < 3 || (argc > 4 && (argv[4] != std::string("-s")))) {
-    std::cerr << usageMsg << std::endl;
-    return 1;
-  }
-  const auto outputFile = std::string(argv[2]);
-
-  bool patching = false;
   UTIL::CheckCollections colPatcher {};
-  std::vector<std::pair<std::string, std::string>> namesTypes;
-  std::set<std::string> subsetColls;
-  int maxEvt = -1;
-
-  if (argc >= 4) {
-    namesTypes = getNamesAndTypes(argv[3]);
+  std::vector<std::pair<std::string, std::string>> namesTypes {};
+  const bool patching = !args.patchFile.empty();
+  if (patching) {
+    namesTypes = getNamesAndTypes(args.patchFile);
     if (namesTypes.empty()) {
       std::cerr << "The provided list of collection names and types does not satisfy the required format: Pair of Name "
                    "and Type per line separated by space"
@@ -96,25 +139,25 @@ int main(int argc, char* argv[])
       return 1;
     }
     colPatcher.addPatchCollections(namesTypes);
-    patching = true;
-
-    if (argc >= 5 && (argv[4] == std::string("-s"))) {
-      // new mode: write only a subset of collection as specified in patch file
-      patching = false;
-      createSubset = true;
-      for (auto& nt : namesTypes) {
-        subsetColls.insert(nt.first);
-      }
-    }
-    if (argc == 6) maxEvt = std::atoi(argv[5]);
   }
+  // Construct a vector of collections to convert. If namesTypes is empty, this
+  // will be empty, and convertEvent will fall back to use the collections in
+  // the event
+  const auto collsToConvert = [&namesTypes]() {
+    std::vector<std::string> names;
+    names.reserve(namesTypes.size());
+    for (const auto& [name, type] : namesTypes) {
+      names.emplace_back(name);
+    }
+    return names;
+  }();
 
   auto lcreader = IOIMPL::LCFactory::getInstance()->createLCReader();
-  lcreader->open(argv[1]);
-  std::cout << "Number of events: " << lcreader->getNumberOfEvents() << '\n';
-  std::cout << "Number of runs: " << lcreader->getNumberOfRuns() << '\n';
+  lcreader->open(args.inputFile);
+  std::cout << "Number of events in file: " << lcreader->getNumberOfEvents() << '\n';
+  std::cout << "Number of runs in file: " << lcreader->getNumberOfRuns() << '\n';
 
-  podio::ROOTFrameWriter writer(outputFile);
+  podio::ROOTFrameWriter writer(args.outputFile);
 
   for (auto j = 0u; j < lcreader->getNumberOfRuns(); ++j) {
     if (j % 1 == 0) {
@@ -126,25 +169,17 @@ int main(int argc, char* argv[])
     writer.writeFrame(edmRunHeader, "runs");
   }
 
-  int nEvt = maxEvt > 0 ? maxEvt : lcreader->getNumberOfEvents();
-
+  const int nEvt = args.nEvents > 0 ? args.nEvents : lcreader->getNumberOfEvents();
   for (auto i = 0u; i < nEvt; ++i) {
     if (i % 10 == 0) {
       std::cout << "processing Event: " << i << std::endl;
     }
-    auto evt = lcreader->readNextEvent(EVENT::LCIO::UPDATE);
+    auto evt = lcreader->readNextEvent();
     // Patching the Event to make sure all events contain the same Collections.
     if (patching == true) {
       colPatcher.patchCollections(evt);
     }
-    if (createSubset) {
-      auto* colNames = evt->getCollectionNames();
-      for (auto& n : *colNames) {
-        if (subsetColls.find(n) == subsetColls.end()) evt->removeCollection(n);
-      }
-    }
-
-    const auto edmEvent = LCIO2EDM4hepConv::convertEvent(evt);
+    const auto edmEvent = LCIO2EDM4hepConv::convertEvent(evt, collsToConvert);
     writer.writeFrame(edmEvent, "events");
   }
 
