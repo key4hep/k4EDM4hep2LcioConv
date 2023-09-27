505ef6f940507a753effd2634b10541399ef7d6b
tmadlener <thomas.madlener@desy.de>
2023-09-27 14:22:49 +0200

    Add the roundtrip tests from k4MarlinWrapper

---
+++ tests/edm4hep_roundtrip.cpp
@@ -0,0 +1,30 @@
+#include "CompareEDM4hepEDM4hep.h"
+#include "EDM4hep2LCIOUtilities.h"
+
+#include "k4EDM4hep2LcioConv/k4EDM4hep2LcioConv.h"
+#include "k4EDM4hep2LcioConv/k4Lcio2EDM4hepConv.h"
+
+#include "podio/Frame.h"
+
+#include <iostream>
+
+#define ASSERT_SAME_OR_ABORT(type, name)                                     \
+  if (!compare(origEvent.get<type>(name), roundtripEvent.get<type>(name))) { \
+    std::cerr << "Comparison failure in " << name << std::endl;              \
+    return 1;                                                                \
+  }
+
+int main()
+{
+  const auto origEvent = createExampleEvent();
+  const auto lcioEvent = EDM4hep2LCIOConv::convEvent(origEvent);
+  const auto roundtripEvent = LCIO2EDM4hepConv::convertEvent(lcioEvent.get());
+
+  ASSERT_SAME_OR_ABORT(edm4hep::CalorimeterHitCollection, "caloHits");
+  ASSERT_SAME_OR_ABORT(edm4hep::MCParticleCollection, "mcParticles");
+  ASSERT_SAME_OR_ABORT(edm4hep::SimCalorimeterHitCollection, "simCaloHits");
+  ASSERT_SAME_OR_ABORT(edm4hep::TrackCollection, "tracks");
+  ASSERT_SAME_OR_ABORT(edm4hep::TrackerHitCollection, "trackerHits");
+
+  return 0;
+}
---
+++ tests/src/CompareEDM4hepEDM4hep.h
@@ -0,0 +1,18 @@
+#ifndef K4EDM4HEP2LCIOCONV_TEST_COMPAREEDM4HEPEDM4HEP_H
+#define K4EDM4HEP2LCIOCONV_TEST_COMPAREEDM4HEPEDM4HEP_H
+
+#include "EDM4hep2LCIOUtilities.h"
+
+bool compare(const edm4hep::CalorimeterHitCollection& origColl, const edm4hep::CalorimeterHitCollection& roundtripColl);
+
+bool compare(const edm4hep::MCParticleCollection& origColl, const edm4hep::MCParticleCollection& roundtripColl);
+
+bool compare(
+  const edm4hep::SimCalorimeterHitCollection& origColl,
+  const edm4hep::SimCalorimeterHitCollection& roundtripColl);
+
+bool compare(const edm4hep::TrackCollection& origColl, const edm4hep::TrackCollection& roundtripColl);
+
+bool compare(const edm4hep::TrackerHitCollection& origColl, const edm4hep::TrackerHitCollection& roundtripColl);
+
+#endif // K4EDM4HEP2LCIOCONV_TEST_COMPAREEDM4HEPEDM4HEP_H
