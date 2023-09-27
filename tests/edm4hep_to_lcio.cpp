ea3ef92d0ef4e193d238f7f313d09b9aa37eed80
tmadlener <thomas.madlener@desy.de>
2023-09-27 10:10:58 +0200

    Add test for EDM4hep to LCIO conversion

--- tests/edm4hep_to_lcio.cpp
+++ tests/edm4hep_to_lcio.cpp
@@ -1,7 +1,11 @@
 #include "EDM4hep2LCIOUtilities.h"
 #include "CompareEDM4hepLCIO.h"
+#include "ObjectMapping.h"
+#include "ComparisonUtils.h"
 
 #include "k4EDM4hep2LcioConv/k4EDM4hep2LcioConv.h"
+
+#include "Exceptions.h"
 
 #include "podio/Frame.h"
 
@@ -12,16 +16,50 @@ struct TD;
 
 int main()
 {
-  auto convObjMaps = EDM4hep2LCIOConv::CollectionsPairVectors {};
-  auto objectMaps = ObjectMappings {};
-
-  const auto caloHitColl = createCalorimeterHits(10);
-  const auto lcioColl = EDM4hep2LCIOConv::convCalorimeterHits(&caloHitColl, "", convObjMaps.calohits);
-
   const auto edmEvent = createExampleEvent();
+  const auto lcioEvent = EDM4hep2LCIOConv::convEvent(edmEvent);
 
-  if (!compare(lcioColl, caloHitColl, objectMaps)) {
-    return 1;
+  // if (!compareEventHeader(lcioEvent.get(), &edmEvent)) {
+  //   return 1;
+  // }
+
+  for (const auto& name : edmEvent.getAvailableCollections()) {
+    const auto edmColl = edmEvent.get(name);
+    const auto typeName = edmColl->getValueTypeName();
+    if (typeName == "edm4hep::CaloHitContribution" || typeName == "edm4hep::ParticleID") {
+      continue;
+    }
+    try {
+      const auto* lcColl = lcioEvent->getCollection(name);
+      if (lcColl->getNumberOfElements() != edmColl->size()) {
+        std::cerr << "Collection " << name << " has different sizes. EDM4hep: " << edmColl->size()
+                  << ", LCIO: " << lcColl->getNumberOfElements() << std::endl;
+        return 1;
+      }
+    } catch (EVENT::DataNotAvailableException& ex) {
+      std::cerr << "Collection " << name << " is not available in LCEvent" << std::endl;
+      return 1;
+    }
+  }
+
+  const auto objectMapping = ObjectMappings::fromEvent(lcioEvent.get(), edmEvent);
+
+  for (const auto& name : edmEvent.getAvailableCollections()) {
+    const auto* lcioColl = lcioEvent->getCollection(name);
+    const auto type = edmEvent.get(name)->getTypeName();
+
+    ASSERT_COMPARE_OR_EXIT(edm4hep::MCParticleCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::ReconstructedParticleCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::TrackCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::TrackerHitCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::TrackerHitPlaneCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::SimTrackerHitCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::CalorimeterHitCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::RawCalorimeterHitCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::SimCalorimeterHitCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::RawTimeSeriesCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::ClusterCollection)
+    ASSERT_COMPARE_OR_EXIT(edm4hep::VertexCollection)
   }
 
   return 0;
