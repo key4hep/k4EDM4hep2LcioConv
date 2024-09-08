97b3a75315b98b0914a5f87c3debb0987e28f98c
jmcarcell <jmcarcell@users.noreply.github.com>
2024-09-08 16:45:04 +0200

    Use the Key4hepConfig flag to set the standard, compiler flags and
    rpath magic.

--- cmake/build_flags.cmake
+++ cmake/build_flags.cmake
@@ -1,36 +1,11 @@
 #---------------------------------------------------------------------------------------------------
-#---k4edm4hep2lcioconv_set_compiler_flags
+#---k4edm4hep2lcioconv_set_linker_flags
 #
-#  Set compiler and linker flags
+#  Set linker flags
 #
 #---------------------------------------------------------------------------------------------------
 
-macro(k4edm4hep2lcioconv_set_compiler_flags)
-  include(CheckCXXCompilerFlag)
-
-  SET(COMPILER_FLAGS -fPIC -Wall -Wextra -Wpedantic)
-
-  # AppleClang/Clang specific warning flags
-  if(CMAKE_CXX_COMPILER_ID MATCHES "^(Apple)?Clang$")
-    set ( COMPILER_FLAGS ${COMPILER_FLAGS} -Winconsistent-missing-override -Wheader-hygiene )
-  endif()
-
-  FOREACH( FLAG ${COMPILER_FLAGS} )
-    ## need to replace the minus or plus signs from the variables, because it is passed
-    ## as a macro to g++ which causes a warning about no whitespace after macro
-    ## definition
-    STRING(REPLACE "-" "_" FLAG_WORD ${FLAG} )
-    STRING(REPLACE "+" "P" FLAG_WORD ${FLAG_WORD} )
-
-    CHECK_CXX_COMPILER_FLAG( "${FLAG}" CXX_FLAG_WORKS_${FLAG_WORD} )
-    IF( ${CXX_FLAG_WORKS_${FLAG_WORD}} )
-      message( STATUS "Adding ${FLAG} to CXX_FLAGS" )
-      SET ( CMAKE_CXX_FLAGS "${FLAG} ${CMAKE_CXX_FLAGS} ")
-    ELSE()
-      message( STATUS "NOT Adding ${FLAG} to CXX_FLAGS" )
-    ENDIF()
-  ENDFOREACH()
-
+macro(k4edm4hep2lcioconv_set_linker_flags)
   # resolve which linker we use
   execute_process(COMMAND ${CMAKE_CXX_COMPILER} -Wl,--version OUTPUT_VARIABLE stdout ERROR_QUIET)
   if("${stdout}" MATCHES "GNU ")
@@ -55,4 +30,4 @@ macro(k4edm4hep2lcioconv_set_compiler_flags)
     MESSAGE( WARNING "No known linker (GNU or Apple) has been detected, pass no flags to linker" )
   endif()
 
-endmacro(k4edm4hep2lcioconv_set_compiler_flags)
+endmacro()
