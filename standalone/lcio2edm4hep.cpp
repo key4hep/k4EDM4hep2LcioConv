779a8bf866c700e14d1c7a059523debac6b88999
Leonhard Reichenbach <Zehvogel@users.noreply.github.com>
2025-02-01 14:09:34 +0100

    fix printout

--- standalone/lcio2edm4hep.cpp
+++ standalone/lcio2edm4hep.cpp
@@ -207,7 +207,7 @@ int main(int argc, char* argv[]) {
   for (int i = 0; i < nEvt; ++i) {
     int tenPercent = nEvt / 10;
     if ((i + 1) % tenPercent == 0) {
-      std::cout << "processed amount of events: " << percEvt << "% (event: " << i << ")" << std::endl;
+      std::cout << "processed amount of events: " << (i + 1) / tenPercent << "% (event: " << i << ")" << std::endl;
     }
     auto evt = lcreader->readNextEvent();
     // Patching the Event to make sure all events contain the same Collections.
