bb575ff79ff1b530198b3308474c5f6ba517f2fc
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2025-12-04 08:46:21 +0000

    Fix roundtrip test by not checking for collections that are not there (#125)

--- tests/edm4hep_roundtrip.cpp
+++ tests/edm4hep_roundtrip.cpp
@@ -19,25 +19,22 @@ int main() {
   const auto lcioEvent = EDM4hep2LCIOConv::convertEvent(origEvent, metadata);
   const auto roundtripEvent = LCIO2EDM4hepConv::convertEvent(lcioEvent.get());
 
-  ASSERT_SAME_OR_ABORT(edm4hep::CalorimeterHitCollection, "caloHits");
-  ASSERT_SAME_OR_ABORT(edm4hep::MCParticleCollection, "mcParticles");
-  ASSERT_SAME_OR_ABORT(edm4hep::SimCalorimeterHitCollection, "simCaloHits");
-  ASSERT_SAME_OR_ABORT(edm4hep::SimTrackerHitCollection, "simTrackerHits");
-  ASSERT_SAME_OR_ABORT(edm4hep::TrackCollection, "tracks");
-  ASSERT_SAME_OR_ABORT(edm4hep::TrackerHit3DCollection, "trackerHits");
-  ASSERT_SAME_OR_ABORT(edm4hep::TrackerHitPlaneCollection, "trackerHitPlanes");
-  ASSERT_SAME_OR_ABORT(edm4hep::TrackerHitSimTrackerHitLinkCollection, "simTrackerHitLinks");
-  ASSERT_SAME_OR_ABORT(edm4hep::ClusterCollection, "clusters");
-  ASSERT_SAME_OR_ABORT(edm4hep::ReconstructedParticleCollection, "recos");
-  ASSERT_SAME_OR_ABORT(edm4hep::ParticleIDCollection, "ParticleID_coll_1");
-  ASSERT_SAME_OR_ABORT(edm4hep::ParticleIDCollection, "ParticleID_coll_2");
-  ASSERT_SAME_OR_ABORT(edm4hep::ParticleIDCollection, "ParticleID_coll_3");
-  ASSERT_SAME_OR_ABORT(edm4hep::RecoMCParticleLinkCollection, "mcRecoLinks");
-  ASSERT_SAME_OR_ABORT(edm4hep::CaloHitSimCaloHitLinkCollection, "mcCaloHitsLinks");
+  ASSERT_SAME_OR_ABORT(edm4hep::CalorimeterHitCollection, "caloHits")
+  ASSERT_SAME_OR_ABORT(edm4hep::MCParticleCollection, "mcParticles")
+  ASSERT_SAME_OR_ABORT(edm4hep::SimCalorimeterHitCollection, "simCaloHits")
+  ASSERT_SAME_OR_ABORT(edm4hep::SimTrackerHitCollection, "simTrackerHits")
+  ASSERT_SAME_OR_ABORT(edm4hep::TrackCollection, "tracks")
+  ASSERT_SAME_OR_ABORT(edm4hep::TrackerHit3DCollection, "trackerHits")
+  ASSERT_SAME_OR_ABORT(edm4hep::TrackerHitPlaneCollection, "trackerHitPlanes")
+  ASSERT_SAME_OR_ABORT(edm4hep::TrackerHitSimTrackerHitLinkCollection, "simTrackerHitLinks")
+  ASSERT_SAME_OR_ABORT(edm4hep::ClusterCollection, "clusters")
+  ASSERT_SAME_OR_ABORT(edm4hep::ReconstructedParticleCollection, "recos")
+  ASSERT_SAME_OR_ABORT(edm4hep::RecoMCParticleLinkCollection, "mcRecoLinks")
+  ASSERT_SAME_OR_ABORT(edm4hep::CaloHitSimCaloHitLinkCollection, "mcCaloHitsLinks")
   ASSERT_SAME_OR_ABORT(edm4hep::RecDqdxCollection, "tracks_dQdx")
-  ASSERT_SAME_OR_ABORT(edm4hep::VertexCollection, "vertices");
-  ASSERT_SAME_OR_ABORT(edm4hep::ReconstructedParticleCollection, "vtx_recos");
-  ASSERT_SAME_OR_ABORT(edm4hep::VertexRecoParticleLinkCollection, "startVtxLinks");
+  ASSERT_SAME_OR_ABORT(edm4hep::VertexCollection, "vertices")
+  ASSERT_SAME_OR_ABORT(edm4hep::ReconstructedParticleCollection, "vtx_recos")
+  ASSERT_SAME_OR_ABORT(edm4hep::VertexRecoParticleLinkCollection, "startVtxLinks")
 
   return 0;
 }
